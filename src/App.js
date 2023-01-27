73372622b8b77801f403602369cca5534a45f4b4
KevinLliguisupa <kevinlliguisupa6@gmail.com>
2023-01-27 09:26:56 -0500

    Incorporacion librearia PrimeFaces

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import ShowProducts from './components/Pruebas/productos/ShowProducts'
+
+import "primereact/resources/themes/lara-light-indigo/theme.css";  //theme
+import "primereact/resources/primereact.min.css";                  //core css
+import "primeicons/primeicons.css";
+   
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <Routes>
+      <Route path = '/pruebas' element = {<ShowProducts></ShowProducts>}></Route>
+    </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
