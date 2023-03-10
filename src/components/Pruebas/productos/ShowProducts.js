f5e2b3f7497696af48436970875c00a7162cfecc
[Stalyn Palma] <[sdpalmaq@utn.edu.ec]>
2023-03-09 21:02:24 -0500

    form of register contract

--- src/components/Pruebas/productos/ShowProducts.js
+++ src/components/Pruebas/productos/ShowProducts.js
@@ -10,45 +10,100 @@ import { Column } from "primereact/column";
 
 const ShowProducts = () => {
 
-    const url = 'http://localhost:8080/productos/';
-    // const url = 'http://localhost:8080/productos?page=0&size=2&enablePagination=true';
-    const [products, setProducts] = useState([]);
+    const [data, setData] = useState([
+        { id: 1, name: 'Producto 1', category: 'Categoría 1' },
+        { id: 2, name: 'Producto 2', category: 'Categoría 2' },
+        { id: 3, name: 'Producto 3', category: 'Categoría 3' },
+    ]);
+    const [editing, setEditing] = useState(false);
+    const [editProduct, setEditProduct] = useState({ id: null, name: '', category: '' });
+    const [categories, setCategories] = useState(['Categoría 1', 'Categoría 2', 'Categoría 3']);
 
-
-    useEffect(() => {
-        getProducts();
-    }, []);
-
-    const getProducts = async () => {
-        const response = await axios.get(url);
-        const productsResponse = response.data.content;
-        productsResponse.map((product) => {
-
-            product.proIva = product.proIva ? "Si" : "No"
-            product.proEstado = product.proEstado ? "Activo" : "Inactivo";
-            return 0;
-        });
-        setProducts(productsResponse);
+    function addProduct(product) {
+        setData([...data, product]);
     }
 
+    function deleteProduct(id) {
+        setData(data.filter((product) => product.id !== id));
+    }
 
+    function updateProduct(id, updatedProduct) {
+        setData(data.map((product) => (product.id === id ? updatedProduct : product)));
+        setEditing(false);
+    }
+
+    function editRow(product) {
+        setEditing(true);
+        setEditProduct({ id: product.id, name: product.name, category: product.category });
+    }
+
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setEditProduct({ ...editProduct, [name]: value });
+    }
+
+    function handleSelectChange(event) {
+        const { value } = event.target;
+        setEditProduct({ ...editProduct, category: value });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        if (editing) {
+            updateProduct(editProduct.id, editProduct);
+        } else {
+            addProduct({ ...editProduct, id: data.length + 1 });
+        }
+        setEditProduct({ id: null, name: '', category: '' });
+        setEditing(false);
+    }
 
     return (
-        <div className="App">
-            <div className="container-fluid">
-
-                <InputText />
-
-                <DataTable value={products} responsiveLayout="scroll" >
-                    <Column field="proId" header="Code"></Column>
-                    <Column field="proNombre" header="Name"></Column>
-                    <Column field="categoria.catNombre" header="Category"></Column>
-                    <Column field="quantity" header="Quantity"></Column>
-                </DataTable>
-
-            </div>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Nombre:
+                    <input type="text" name="name" value={editProduct.name} onChange={handleInputChange} />
+                </label>
+                <label>
+                    Categoría:
+                    <select value={editProduct.category} onChange={handleSelectChange}>
+                        {categories.map((category) => (
+                            <option value={category} key={category}>
+                                {category}
+                            </option>
+                        ))}
+                    </select>
+                </label>
+                <button>{editing ? 'Actualizar' : 'Agregar'}</button>
+                {editing && <button onClick={() => setEditing(false)}>Cancelar</button>}
+            </form>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Categoría</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((product) => (
+                        <tr key={product.id}>
+                            <td>{product.id}</td>
+                            <td>{product.name}</td>
+                            <td>{product.category}</td>
+                            <td>
+                                <button onClick={() => editRow(product)}>Editar</button>
+                                <button onClick={() => deleteProduct(product.id)}>Eliminar</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
-    )
+    );
 }
 
 export default ShowProducts
--- src/components/contratos/registro/registros.jsx
+++ src/components/contratos/registro/registros.jsx
@@ -1,24 +1,163 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { DataTable } from 'primereact/datatable';
+import { Toast } from 'primereact/toast';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
 import { Toolbar } from 'primereact/toolbar';
-
+import { InputText } from 'primereact/inputtext';
+import { InputTextarea } from 'primereact/inputtextarea';
+import { Dialog } from 'primereact/dialog';
+import { RadioButton } from 'primereact/radiobutton';
+import { InputNumber } from 'primereact/inputnumber';
+import { Dropdown } from 'primereact/dropdown';
 import './registro.css'
 
 const ContratRegister = () => {
 
     const url = 'http://localhost:4000/nominaweb/api/v1/contrato/contratos';
+
+
+    const emptyContract = {
+        con_id: "",
+        con_fecha_entrada: null,
+        con_fecha_salida: null,
+        con_liquidacion_estado: true,
+        con_liquidacion_fecha: null,
+        con_liquidacion_observacion: "",
+        con_estado: true,
+        cont_emp: {
+            emp_cedula: ""
+        },
+        pue_id: "",
+        globalFilter: null
+    }
+
     const [contracts, setContracts] = useState({});
+    const [contractDialog, setContractDialog] = useState(false);
+    const [deleteContractDialog, setdeleteContractDialog] = useState(false);
+    const [deleteContractsDialog, setDeleteContractsDialog] = useState(false);
+    const [contract, setContract] = useState(emptyContract);
+    const [selectedContracts, setSelectedContracts] = useState(null);
+    const [submitted, setSubmitted] = useState(false);
+    const [globalFilter, setGlobalFilter] = useState(null);
 
-    
+    //employees
+    const [employees, setEmployees] = useState({});
+    const [selectedEmployees, setSelectedEmployees] = useState(null);
+
+    function formatCurrency(value) {
+        return value.toLocaleString("en-US", { style: "currency", currency: "USD" });
+    }
+
+    const openNew = () => {
+        setContract(emptyContract);
+        setSubmitted(false);
+        setContractDialog(true);
+    }
+
+    const hideDialog = () => {
+        setSubmitted(false);
+        setContractDialog(false);
+    }
+
+    const hideDeleteContractDialog = () => {
+        setdeleteContractDialog(false);
+    }
+
+    const hideDelectedContractsDialog = () => {
+        setDeleteContractsDialog(false);
+    }
+
+    const saveContract = () => {
+        setSubmitted(true);
+        if (contract.con_id.trim()) {
+            let _contracts = [...contracts];
+            let _contract = { ...contract };
+            if (contract.con_id) {
+                const index = findIndexById(contract.con_id);
+                _contracts[index] = _contract;
+                // showToast('success', 'Product Updated', 'Product has been updated successfully.');
+            }
+            else {
+                _contract.con_id = createId();
+                _contracts.push(_contract);
+                // showToast('success', 'Product Created', 'Product has been created successfully.');
+            }
+
+            setContracts(_contracts);
+            setContractDialog(false);
+            setContract(emptyContract);
+        }
+    }
+
+    const editContract = (contract) => {
+        setContract({ ...contract });
+        setContractDialog(true);
+        console.log(contract)
+    }
+
+    const confirmDeleteContract = (contract) => {
+        setContract(contract);
+        setdeleteContractDialog(true);
+    }
+
+    const onEmployeeChange = (e) => {
+        console.log('d',e.value)
+        setContract({
+            ...contract, cont_emp: {
+                emp_cedula: e.value
+            }
+        });
+
+    }
+
+    const deleteContract = () => {
+        let _contracts = contracts.filter(val => val.id !== contract.id);
+        setContracts(_contracts);
+        setdeleteContractDialog(false);
+        setContract(emptyContract);
+        // showToast('success', 'Product Deleted', 'Product has been deleted successfully.');
+    }
+
+    const deleteSelectedContracts = () => {
+        let _contracts = contracts.filter(val => !selectedContracts.includes(val));
+        setContracts(_contracts);
+        setDeleteContractsDialog(false);
+        setSelectedContracts(null);
+        // showToast('success', 'Products Deleted', 'Selected products have been deleted successfully.');
+    }
+
+    const findIndexById = (id) => {
+        let index = -1;
+        for (let i = 0; i < contracts.length; i++) {
+            if (contracts[i].id === id) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+
+    const createId = () => {
+        let id = '';
+        let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        for (let i = 0; i < 5; i++) {
+            id += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        return id;
+    }
+
+    const onInputChange = (e, name) => {
+        const val = (e.target && e.target.value) || '';
+        let _contract = { ...contract };
+        _contract[`${name}`] = val;
+
+        setContract(_contract);
+    }
 
 
-    useEffect(() => {
-        getContracts();
-    }, []);
-
+    //Consumo de API
     const getContracts = async () => {
         const response = await axios.get(url);
 
@@ -26,36 +165,98 @@ const ContratRegister = () => {
         setContracts(response.data);
     }
 
-    //actions 
-    const openNew =()=>{
+    const getEmployees = async () => {
+        const response = await axios.get('http://localhost:4000/nominaweb/api/v1/empleado/empleados');
 
+        console.log('e', response.data)
+        setEmployees(response.data);
     }
+
+
+    useEffect(() => {
+        getContracts();
+    }, []);
+
+    useEffect(() => {
+        getEmployees();
+    }, []);
+
+
     //components
 
-    const header = (
-        <div className="flex flex-wrap align-items-center justify-content-between gap-2">
-            <span className="text-xl text-900 font-bold">Contratos</span>
-        </div>
-    );
 
     const leftToolbarTemplate = (
         <React.Fragment>
-            <Button label="New" icon="pi pi-plus" className="p-button-success p-mr-2"  />
-            <Button label="Delete" icon="pi pi-trash" className="p-button-danger"  />
+            <Button label="New" icon="pi pi-plus" className="p-button-success p-mr-2" onClick={openNew} />
+            <Button label="Delete" icon="pi pi-trash" className="p-button-danger" />
         </React.Fragment>
     )
 
+    const header = (
+        <div className="table-header">
+            <h5 className="p-m-0">Listado de Contratos</h5>
+        </div>
+    );
+    const productDialogFooter = (
+        <React.Fragment>
+            <Button label="Cancel" icon="pi pi-times" className="p-button-text" onClick={hideDialog} />
+            <Button label="Save" icon="pi pi-check" className="p-button-text" onClick={saveContract} />
+        </React.Fragment>
+    );
+    const deleteProductDialogFooter = (
+        <React.Fragment>
+            <Button label="No" icon="pi pi-times" className="p-button-text" onClick={hideDeleteContractDialog} />
+            <Button label="Yes" icon="pi pi-check" className="p-button-text" onClick={deleteContract} />
+        </React.Fragment>
+    );
+    const deleteProductsDialogFooter = (
+        <React.Fragment>
+            <Button label="No" icon="pi pi-times" className="p-button-text" onClick={hideDelectedContractsDialog} />
+            <Button label="Yes" icon="pi pi-check" className="p-button-text" onClick={deleteSelectedContracts} />
+        </React.Fragment>
+    );
+    const actionBodyTemplate = (rowData) => {
+        // Action body template logic
+        return (
+            <React.Fragment>
+                <Button icon="pi pi-pencil" className="p-button-rounded p-button-success p-mr-2" onClick={() => editContract(rowData)} />
+                <Button icon="pi pi-trash" className="p-button-rounded p-button-warning" onClick={() => confirmDeleteContract(rowData)} />
+            </React.Fragment>
+        );
+    };
+
     return (
-        <div className="card">
-            <Toolbar className="p-mb-4" left={leftToolbarTemplate} ></Toolbar>
-            <DataTable value={contracts} header={header} tableStyle={{ minWidth: '60rem' }}>
-                <Column field="con_id" header="Code"></Column>
-                <Column field="cont_emp.emp_cedula" header=" Cedula Empleado"></Column>
-                <Column field="cont_emp.emp_nombres" header=" Nombres Empleado"></Column>
-                <Column field="cont_emp.emp_apellidos" header=" Apellidos Empleado"></Column>
-                <Column field="con_liquidacion_estado" header="Category"></Column>
-                <Column field="con_liquidacion_fecha" header="Quantity"></Column>
-            </DataTable>
+        <div className="datatable-crud-demo">
+
+
+            <div className="card">
+                <Toolbar className="p-mb-4" left={leftToolbarTemplate} ></Toolbar>
+
+                <DataTable value={contracts} selection={selectedContracts} onSelectionChange={(e) => setSelectedContracts(e.value)}
+                    dataKey="con_id"
+                    header={header}>
+                    <Column selectionMode="multiple" headerStyle={{ width: '3rem' }}></Column>
+                    <Column field="con_id" header="Code" sortable></Column>
+                    <Column field= {(employees) => `${employees.cont_emp.emp_cedula} - ${employees.cont_emp.emp_nombres} ${employees.cont_emp.emp_apellidos}`} header="Empleado" sortable></Column>
+                    <Column field="con_fecha_entrada" header="Fecha Entrada" sortable></Column>
+                    <Column field="con_fecha_salida" header="Fecha Salida" sortable></Column>
+                    <Column field="con_liquidacion_estado" header="Category" sortable></Column>
+                    <Column body={actionBodyTemplate}></Column>
+                </DataTable>
+            </div>
+            <Dialog visible={contractDialog} style={{ width: '450px' }} header="Product Details" modal className="p-fluid" footer={productDialogFooter} onHide={hideDialog}>
+                <div className="p-field">
+                    <label htmlFor="name">Codigo Prueba</label>
+                    <InputText id="name" value={contract.con_id} onChange={(e) => onInputChange(e, 'con_id')} required autoFocus />
+                </div>
+                <div className="p-field">
+                    <label htmlFor="description">Seleccione Empleado</label>
+                    <Dropdown name="emp_cedula" value={contract.cont_emp.emp_cedula} onChange={onEmployeeChange} options={employees} optionLabel={(employees) => `${employees.emp_cedula} - ${employees.emp_nombres} ${employees.emp_apellidos}`} placeholder="Seleccione un empleado"
+                        filter className="w-full md:w-14rem" optionValue="emp_cedula" />
+                </div>
+            </Dialog>
+
+
         </div>
     );
 };
